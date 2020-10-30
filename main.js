85da5361288811866948571b045a85ed497ab675
EvertonWingert <54149497+EvertonWingert@users.noreply.github.com>
2020-10-29 23:00:56 -0300

    Novos recursos
    *Sistema de busca por ID e NOME do pokemon
    *Paginação com opções de proximo e anterior

--- main.js
+++ main.js
@@ -1,50 +1,112 @@
+//Variaveis
+var listaPokemons = document.getElementById("pokemons");
+var pokemons = [];
+var initPage = 1;
+var perPage = 12;
+var lastPage = perPage;
 
-const POKEMONS_NUMBER = 150;
-var div = document.getElementById('pokemons');
 
-async function fetchPokemons(){
-  for(let i = 1; i <= POKEMONS_NUMBER; i++){
-    await getPokemon(i);
-  } 
+
+//Inicialização
+getPokemons();
+
+//Controle da paginação
+function next(){
+
+  initPage = lastPage + 1;
+  lastPage = lastPage + perPage;
+  console.log(initPage,lastPage);
+
+  if(initPage > 250){
+    return
+  }
+
+  listaPokemons.innerHTML = '';
+  pokemons = [];
+  getPokemons();
+}
+function previus(){
+
+  initPage = initPage - perPage;
+  lastPage = lastPage - perPage;
+
+  if(initPage < 1){
+    return
+  }
+  listaPokemons.innerHTML = '';
+  pokemons = [];
+  getPokemons();
+  
+}
+//Metodos
+async function getPokemons() {
+  for (let i = initPage; i <= lastPage; i++) {
+    pokemons.push(await fetchPokemons(i));
+  }
+  renderAll();
 }
 
-async function getPokemon(id)   {
-  const URL =  `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+async function fetchPokemons(id) {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const response = await fetch(URL);
   const pokemon = await response.json();
-  render(pokemon); 
+  return pokemon;
+  
 }
 
-function getTypes(types){
+function getTypes(pokemon) {
+  const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
   let spanHtml = ``;
-  types.forEach(element => {
-    spanHtml += `<span class="badge badge-primary text-capitalize m-1 ${element}">${element}</span>`
+  types.forEach((element) => {
+    spanHtml += `<span class="badge badge-primary text-capitalize m-1 ${element}">${element}</span>`;
   });
 
   return spanHtml;
 }
 
+function renderAll(){
+  pokemons.forEach((pokemon) => {
+    render(pokemon);
+  })
+}
 function render(pokemon){
-    const types = pokemon.types.map(typeInfo => typeInfo.type.name);
-    let typesHTML = getTypes(types);
-
-    div.innerHTML += 
+    let typesHTML = getTypes(pokemon);
+    listaPokemons.innerHTML += 
     `
-    <li>
-      <div class="card p-2 m-2 shadow-sm border-0 " style="width: 18rem;">
-      <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
-      <div class="card-body">
-        <h5 class="card-tittle text-center text-capitalize">${pokemon.name}</h5> 
-        ${typesHTML}
-      </div>
-    </div> 
-    </li>
-    
-    ` 
-   
+      <li class="p-2 m-2 ">
+        <div class="card  border-0 shadow-sm" style="width: 18rem;">
+          <div class="card-header">
+            <h6 class="card-subtitle mb-2 text-muted">Nº ${pokemon.id}</h6>
+          </div>
+          <div class="card-body">
+            <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
+            <div class="card-body">
+              <h5 class="card-tittle text-center text-capitalize">${pokemon.name}</h5>
+              ${typesHTML}
+            </div>
+          </div>
+          
+      </div> 
+      </li>
+    `;
 }
 
 
-fetchPokemons();
+
+async function search(){
+  let inputValue = document.getElementById('input').value;
+  listaPokemons.innerHTML = '';
+
+  if(inputValue == '') renderAll();
+
+  const pokemon = await fetchPokemons(inputValue.toLowerCase());
+
+  render(pokemon);
+
+}
+
+
+
 
 

57d14948931be5f808a3a49537ea2ecb046d76a2
EvertonWingert <54149497+EvertonWingert@users.noreply.github.com>
2020-10-31 15:34:45 -0300

    Refatorando

--- main.js
+++ main.js
@@ -1,53 +1,38 @@
 //Variaveis
-var listaPokemons = document.getElementById("pokemons");
-var pokemons = [];
-var initPage = 1;
+var pokemonsUl = document.getElementById('pokemons');
+const form = document.querySelector('form');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+
+var pokemonList = [];
+var firstCard = 1;
 var perPage = 12;
-var lastPage = perPage;
-
-
+var lastCard = perPage;
 
 //Inicialização
-getPokemons();
+init();
 
-//Controle da paginação
-function next(){
+//Eventos
+form.addEventListener('submit', event =>{
+  event.preventDefault();
+  const inputValue = event.target.search.value;
+  console.log(inputValue);
+  search(inputValue);
+});
 
-  initPage = lastPage + 1;
-  lastPage = lastPage + perPage;
-  console.log(initPage,lastPage);
+nextButton.addEventListener('click',next);
+previousButton.addEventListener('click',previous);
 
-  if(initPage > 250){
-    return
-  }
 
-  listaPokemons.innerHTML = '';
-  pokemons = [];
-  getPokemons();
-}
-function previus(){
-
-  initPage = initPage - perPage;
-  lastPage = lastPage - perPage;
-
-  if(initPage < 1){
-    return
-  }
-  listaPokemons.innerHTML = '';
-  pokemons = [];
-  getPokemons();
-  
-}
 //Metodos
 async function getPokemons() {
-  for (let i = initPage; i <= lastPage; i++) {
-    pokemons.push(await fetchPokemons(i));
+  for (let i = firstCard; i <= lastCard; i++) {
+    pokemonList.push(await fetchPokemon(i));
   }
-  renderAll();
+  render();
 }
 
-
-async function fetchPokemons(id) {
+async function fetchPokemon(id) {
   const URL = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const response = await fetch(URL);
   const pokemon = await response.json();
@@ -59,52 +44,93 @@ function getTypes(pokemon) {
   const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
   let spanHtml = ``;
   types.forEach((element) => {
-    spanHtml += `<span class="badge badge-primary text-capitalize m-1 ${element}">${element}</span>`;
+    spanHtml += `<span class="badge badge-primary text-capitalize m-1 p-2 ${element}">${element}</span>`;
   });
 
   return spanHtml;
 }
 
-function renderAll(){
-  pokemons.forEach((pokemon) => {
-    render(pokemon);
-  })
+function isPokemon(pokemon){
+  return pokemon == pokemonList;
 }
-function render(pokemon){
-    let typesHTML = getTypes(pokemon);
-    listaPokemons.innerHTML += 
+
+async function search(value){
+  pokemonsUl.innerHTML = '';
+
+  if(value == '') {
+    pokemonList = [];
+    getPokemons();
+    render();
+    return
+  }
+
+  try{
+    const pokemon = await fetchPokemon(value.toLowerCase());
+    pokemonList = [];
+    pokemonList.push(pokemon);
+    render();
+  }catch(e){
+    pokemonsUl.innerHTML = `
+    <div class="alert alert-danger" role="alert">
+      Alguma coisa deu errado!
+    </div>
     `
-      <li class="p-2 m-2 ">
-        <div class="card  border-0 shadow-sm" style="width: 18rem;">
-          <div class="card-header">
-            <h6 class="card-subtitle mb-2 text-muted">Nº ${pokemon.id}</h6>
-          </div>
-          <div class="card-body">
-            <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
-            <div class="card-body">
-              <h5 class="card-tittle text-center text-capitalize">${pokemon.name}</h5>
-              ${typesHTML}
+  }
+}
+
+
+function render(){
+    pokemonList.forEach((pokemon) =>{
+      let typesHTML = getTypes(pokemon);
+      pokemonsUl.innerHTML += 
+      `
+        <li class="p-2 m-2 col-md-auto">
+          <div class="card  border-0 shadow-sm" style="width: 18rem;">
+            <div class="card-header">
+              <h6 class="card-subtitle mb-2 text-muted">Nº ${pokemon.id}</h6>
             </div>
-          </div>
-          
-      </div> 
-      </li>
-    `;
+            <div class="card-body">
+              <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
+              <div class="card-body">
+                <h5 class="card-tittle text-center text-capitalize">${pokemon.name}</h5>
+                ${typesHTML}
+              </div>
+            </div>
+            
+        </div> 
+        </li>
+      `
+    });
+}
+function init(){
+  pokemonsUl.innerHTML = '';
+  pokemonList = [];
+  getPokemons();   
 }
 
 
+//Controle da paginação
+function next(){
+  firstCard = lastCard + 1;
+  lastCard = lastCard + perPage;
 
-async function search(){
-  let inputValue = document.getElementById('input').value;
-  listaPokemons.innerHTML = '';
+  if(firstCard > 250){
+    return
+  }
 
-  if(inputValue == '') renderAll();
-
-  const pokemon = await fetchPokemons(inputValue.toLowerCase());
-
-  render(pokemon);
-
+  init();
 }
+function previous(){
+  firstCard = firstCard - perPage;
+  lastCard = lastCard - perPage;
+
+  if(firstCard < 1){
+    return
+  }
+  init()
+}
+
+
 
 
 
